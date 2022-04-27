f4d5d85f70cb649b0df4f98a451c0d827a230be3
Ayoub Babanou <ayoubbabanou@gmail.com>
2022-04-27 15:38:19 +0200

    tp docker

---
+++ website/index.php
@@ -0,0 +1,19 @@
+
+<html>
+    <head>
+        <title>POZOS</title>
+    </head>
+
+    <body>
+        <h1>Student Checking App</h1>
+        <ul>
+            <form action="" method="POST">
+            <!--<label>Enter student name:</label><br />
+            <input type="text" name="" placeholder="Student Name" required/>
+            <br /><br />-->
+            <button type="submit" name="submit">List Student</button>
+            </form>
+
+        </ul>
+    </body>
+</html>
